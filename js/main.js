353f7060dc76ef858365c661ef2bb110e562d437
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-02 09:49:38 +0200

    upgrade loop commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 console.log('ciao');
 
 
-for (let i = 1; i <=100; i++) {
-    console.log(i);    
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    if (i % 3 == 0)
+        console.log('Fizz');
+    else if (i % 5 == 0)
+        console.log('Buzz');
 }
